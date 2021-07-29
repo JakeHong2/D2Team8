052e9103e5b03de6de2033dcaf5f3019dccee350
exp01 <exp01@DESKTOP-LQL59IT>
2021-07-29 18:47:16 +0900

    Test code refactoring

--- src/com/holub/database/HTMLExporterTest.java
+++ src/com/holub/database/HTMLExporterTest.java
@@ -6,53 +6,68 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class HTMLExporterTest {
+	private Table.Exporter html;
+	private MockWriter writer;
 
-	@Test
-	void test() throws IOException {
-		class MockWriter extends Writer {
-			String resultString = "";
+	class MockWriter extends Writer {
+		String resultString = "";
 
-			@Override
-			public void write(char[] cbuf, int off, int len) throws IOException {
-				char[] arr = new char[len];
-				for (int i = 0; i < len; ++i) {
-					arr[i] = cbuf[off+i];
-				}
-				String line = new String(arr);
-				resultString += line;
+		@Override
+		public void write(char[] cbuf, int off, int len) throws IOException {
+			char[] arr = new char[len];
+			for (int i = 0; i < len; ++i) {
+				arr[i] = cbuf[off+i];
 			}
-
-			@Override
-			public void flush() throws IOException {
-			}
-
-			@Override
-			public void close() throws IOException {
-			}
+			String line = new String(arr);
+			resultString += line;
 		}
-		
-		MockWriter writer = new MockWriter();
-		Table.Exporter html = new HTMLExporter(writer);
+
+		@Override
+		public void flush() throws IOException {
+		}
+
+		@Override
+		public void close() throws IOException {
+		}
+	}
+
+	@BeforeEach
+	public void setUp() {
+		writer = new MockWriter();
+		html = new HTMLExporter(writer);
+	}
+	
+	@Test
+	void testStartEndTable() throws IOException {
 		html.startTable();
-		Object[] columns = new String[] {"last", "first", "addrId"};
-		Iterator<Object> iterator = Arrays.asList(columns).iterator();
-		html.storeMetadata(null, 0, 0, iterator);
-		html.storeRow(Arrays.asList(new Object[] {"Holub", "Allen", 1}).iterator());
-		html.storeRow(Arrays.asList(new Object[] {"Flintstone","Wilma","2"}).iterator());
-		html.storeRow(Arrays.asList(new Object[] {"Flintstone","Fred","2"}).iterator());
 		html.endTable();
 		String expected = "<html>\r\n"
 				+ "<table border=1>\r\n"
-				+ "<tr><th>last</th><th>first</th><th>addrId</th></tr>\r\n"
-				+ "<tr><td>Holub</td><td>Allen</td><td>1</td></tr>\r\n"
-				+ "<tr><td>Flintstone</td><td>Wilma</td><td>2</td></tr>\r\n"
-				+ "<tr><td>Flintstone</td><td>Fred</td><td>2</td></tr>\r\n"
 				+ "</table>\r\n"
 				+ "</html>";
 		Assert.assertEquals(expected, writer.resultString);
 	}
 
+	void testMetadata() throws IOException {
+		Object[] columns = new String[] {"last", "first", "addrId"};
+		Iterator<Object> iterator = Arrays.asList(columns).iterator();
+		html.storeMetadata(null, 0, 0, iterator);
+		String expected = "<tr><th>last</th><th>first</th><th>addrId</th></tr>\r\n";
+		Assert.assertEquals(expected, writer.resultString);
+	}
+
+	void testRows() throws IOException {
+		html.storeRow(Arrays.asList(new Object[] {"Holub", "Allen", 1}).iterator());
+		html.storeRow(Arrays.asList(new Object[] {"Flintstone","Wilma","2"}).iterator());
+		html.storeRow(Arrays.asList(new Object[] {"Flintstone","Fred","2"}).iterator());
+		String expected = "<tr><th>last</th><th>first</th><th>addrId</th></tr>\r\n"
+				+ "<tr><td>Holub</td><td>Allen</td><td>1</td></tr>\r\n"
+				+ "<tr><td>Flintstone</td><td>Wilma</td><td>2</td></tr>\r\n"
+				+ "<tr><td>Flintstone</td><td>Fred</td><td>2</td></tr>\r\n";
+		Assert.assertEquals(expected, writer.resultString);
+	}
 }
